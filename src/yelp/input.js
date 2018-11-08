fb7a021aeb88f7ed63aae50b12165828fd88d36b
NeedsAUsername <kevinchi118@gmail.com>
2018-11-07 20:44:52 -0500

    creates state for yelp input

--- src/yelp/input.js
+++ src/yelp/input.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class YelpInput extends React.Component {
+  state = {
+    location: '11354',
+    price: '2'
+  }
   render () {
     return (
       <form>
-        <input type="text" value="yelp form placeholder" />
-        <input type="submit" />
+        <input type="text" value={this.state.location} />
+        <input type="number" value={this.state.price} />
+        <input type="submit" value="Search" />
       </form>
     )
   }

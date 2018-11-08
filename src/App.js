744aec00b242cd503fc0f65cbae5035bc2a2335c
NeedsAUsername <kevinchi118@gmail.com>
2018-11-07 20:07:44 -0500

    adds yelp container to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YelpContainer from './yelp/container'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Food Picker</h1>
+        <YelpContainer />
       </div>
     );
   }
--- src/yelp/container.js
+++ src/yelp/container.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import YelpInput from './input';
+import YelpIndex from './index';
 
 class YelpContainer extends React.Component {
   render () {
     return (
       <div>
         Yelp Container
+        <YelpInput />
+        <YelpIndex />
       </div>
     )
   }

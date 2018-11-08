109b457b206b69ccafe56e3750e5e8fcb6f4a85a
NeedsAUsername <kevinchi118@gmail.com>
2018-11-07 19:52:49 -0500

    moves input from user to yelp

---
+++ src/yelp/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class YelpInput extends React.Component {
+  render () {
+    return (
+      <form>
+        <input type="text" value="yelp form placeholder" />
+        <input type="submit" />
+      </form>
+    )
+  }
+}
+
+export default YelpInput;

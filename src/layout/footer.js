9ec96a4db355086e82101c881e6030648ed5b6ff
NeedsAUsername <kevinchi118@gmail.com>
2018-11-09 17:42:44 -0500

    adds rel="noopener noreferrer" to target="_blank" links, and adds alt for image

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const footer = () => (
-  <div><a href="https://github.com/NeedsAUsername/food-picker" target="_blank">Check Out This Project's Github Page</a></div>
+  <div><a href="https://github.com/NeedsAUsername/food-picker" target="_blank" rel="noopener noreferrer">Check Out This Project's Github Page</a></div>
 )
 
 export default footer;
--- src/search/component.js
+++ src/search/component.js
@@ -6,9 +6,9 @@ class SearchComponent extends React.Component {
     const rest = this.props.restaurant;
     return (
       <div>
-        <img width="50" height="40" src={rest.image_url} alt="restaurant" />
-        <a href={rest.url} target="_blank">{rest.name}</a>
-        <a href={rest.url} target="_blank"><img src={yelpLogo} /></a>
+        <img width="50" height="40" src={rest.image_url} alt="restaurant-pic-missing" />
+        <a href={rest.url} target="_blank" rel="noopener noreferrer">{rest.name}</a>
+        <a href={rest.url} target="_blank" rel="noopener noreferrer"><img src={yelpLogo} alt="Yelp-Logo-Missing"/></a>
         <div>
           {rest.location.display_address.join(' ')}
         </div>

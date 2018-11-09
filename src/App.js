ac43e293c15a4ce88930639599677c8ecd5e7d07
NeedsAUsername <kevinchi118@gmail.com>
2018-11-08 23:13:40 -0500

    adds footer with github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchContainer from './search/container';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import RandomizerContainer from './randomizer/container';
 import Navbar from './layout/navbar';
+import Footer from './layout/footer';
 
 const testing = () => {
   return (<div>testing</div>)
@@ -18,6 +19,7 @@ class App extends Component {
           <Navbar />
           <Route exact path='/' component={SearchContainer} />
           <Route exact path='/random' component={RandomizerContainer} />
+          <Footer />
         </div>
       </Router>
     );
---
+++ src/layout/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const footer = () => (
+  <div><a href="https://github.com/NeedsAUsername/food-picker" target="_blank">Check Out This Project's Github Page</a></div>
+)
+
+export default footer;

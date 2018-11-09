278b77080765efd61a8639066b55cee891cd3986
NeedsAUsername <kevinchi118@gmail.com>
2018-11-08 21:08:43 -0500

    Adds Navlinks to navbar

--- src/static/navbar.js
+++ src/static/navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 class Navbar extends React.Component {
+  current = () => ({
+    border: 'solid'
+  })
   render () {
     return (
       <nav>
         <h1>Food Picker</h1>
+        <NavLink to="/" exact activeStyle={this.current()}>Search</NavLink>
+        <NavLink to="/random" exact activeStyle={this.current()}>Randomize</NavLink>
       </nav>
     )
   }

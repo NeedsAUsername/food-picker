0cb886e86e0e46fca35369075b206b31b74d40cc
NeedsAUsername <kevinchi118@gmail.com>
2018-11-10 22:24:34 -0500

    removes logo file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {connect} from 'react-redux';

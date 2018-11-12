1e7e16c97c75224b45c3bd071d11d05139f1e77c
NeedsAUsername <kevinchi118@gmail.com>
2018-11-11 22:53:09 -0500

    creates handleChange

--- src/authentication/login.js
+++ src/authentication/login.js
@@ -5,13 +5,20 @@ class Login extends React.Component {
     username: '',
     password: ''
   }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    }, () => console.log(this.state))
+  } 
   render () {
     return (
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username"/>
+        <input type="text" name="username" id="username" value={this.state.username}
+          onChange={this.handleChange}/>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password"/>
+        <input type="password" name="password" id="password" value={this.state.password}
+          onChange={this.handleChange}/>
         <input type="submit" placeholder="submit"/>
       </form>
     )

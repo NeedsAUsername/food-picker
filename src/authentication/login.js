351350a34b8d53e61744e00bc3fb846fa600e92b
NeedsAUsername <kevinchi118@gmail.com>
2018-11-11 22:15:37 -0500

    adds login component with basic form

---
+++ src/authentication/login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render () {
+    return (
+      <form>
+        <label htmlFor "username">Username</label>
+        <input type="text" id="username"/>
+        <label htmlFor "password">Password></label>
+        <input type="password" id="password"/>
+        <input type="submit" placeholder="submit"/>
+      </form>
+    )
+  }
+}
+
+export default Login;

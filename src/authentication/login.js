31b1cfef1b427e5d1a111f0fee411b0de7be7d8d
NeedsAUsername <kevinchi118@gmail.com>
2018-11-11 22:50:17 -0500

    adds name to form and state to component

--- src/authentication/login.js
+++ src/authentication/login.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  }
   render () {
     return (
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username"/>
+        <input type="text" name="username" id="username"/>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password"/>
+        <input type="password" name="password" id="password"/>
         <input type="submit" placeholder="submit"/>
       </form>
     )

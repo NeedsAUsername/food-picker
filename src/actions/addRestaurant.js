d7e9ad8d168059817c8e1c76270c6244fbf2e6c2
NeedsAUsername <kevinchi118@gmail.com>
2018-11-14 22:29:43 -0500

    addRestaurant action now posts to the api to add the restaurant to the db

--- src/actions/addRestaurant.js
+++ src/actions/addRestaurant.js
@@ -1,4 +1,19 @@
 export function addRestaurant(restaurantId) {
+  const url = `${process.env.REACT_APP_RAILS_API}/restaurants`;
+  const options = {
+    method: 'POST',
+    accepts: 'application/json',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-User-Email': localStorage.getItem('email'),
+      'X-User-Token': localStorage.getItem('token')
+    },
+    body: JSON.stringify({restaurantId: restaurantId})
+  }
+  fetch(url, options)
+  .then(response => response.json())
+  .then(json => console.log(json))
+
   return {
       type: 'ADD_RESTAURANT',
       payload: restaurantId

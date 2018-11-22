54a25b70c64307a253749bf4f8290b357193ee5d
NeedsAUsername <kevinchi118@gmail.com>
2018-11-22 14:43:11 -0500

    adds restaurant fetch to the login action.

--- src/actions/user/login.js
+++ src/actions/user/login.js
@@ -16,7 +16,33 @@ export function login(formState) {
         type: 'LOG_IN',
         payload: json
       })
+      if (json.restaurants.length > 0) {
+        json.restaurants.forEach(rest => fetchRestaurantById(rest.yelpNumber, dispatch))
+      }
     })
     .catch(error => console.log(error))
   }
+}
+
+function fetchRestaurantById(id, dispatch) {
+  dispatch({
+    type: 'LOADING_USER_RESTAURANT'
+  })
+  fetch(`${process.env.REACT_APP_PROXY}/https://api.yelp.com/v3/businesses/${id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // If Yelp cannot find it, it will return an error
+    if (data.error) {
+      alert(data.error.description + 'Id: ' + id);
+    } else {
+      dispatch({
+        type: 'FETCH_RESTAURANT_BY_ID',
+        payload: data,
+      })
+    }
+  })
 }

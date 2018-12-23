dee4f7260d27b086c391d6913da4fb9d1313e5d2
Kevin Chi <kevinchi118@gmail.com>
2018-12-22 23:19:27 -0500

    uses scrollIntoView to scroll down to restaurants upon search

--- src/restaurants/index.js
+++ src/restaurants/index.js
@@ -3,24 +3,30 @@ import Spinner from 'react-spinkit';
 import Restaurant from './component';
 
 class Restaurants extends React.Component {
-  renderRestaurants = () => (
-    this.props.restaurants.map(restaurant =>
-    <Restaurant key={restaurant.id} restaurant={restaurant}
-      userRestaurants= {this.props.userRestaurants}
-      addRestaurant={() => this.props.addRestaurant(restaurant)}
-      deleteRestaurant={() => this.props.deleteRestaurant(restaurant.id)}
-    />)
-  )
+  renderRestaurants = () => {
+    return this.props.restaurants.map(restaurant =>
+      <Restaurant key={restaurant.id} restaurant={restaurant}
+        userRestaurants= {this.props.userRestaurants}
+        addRestaurant={() => this.props.addRestaurant(restaurant)}
+        deleteRestaurant={() => this.props.deleteRestaurant(restaurant.id)}
+      />
+    )
+  }
   renderLoadingIcon = () => (
     <div className="icon-wrapper">
       Loading Restaurants...
       <Spinner name="circle" color="blue" fadeIn="none"/>
     </div>
   )
-
+  componentDidUpdate() {
+    let rest = document.querySelector('.content');
+    if (this.props.restaurants && rest) {
+      rest.scrollIntoView()
+    }
+  }
   render () {
     return (
-      <div>
+      <div className="content">
         {this.props.loading ? this.renderLoadingIcon() : this.renderRestaurants()}
       </div>
     )
